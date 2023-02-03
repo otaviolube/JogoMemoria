2d4c5ef936dd6489804a662da86ceab1b7017c39
Otávio Lube <otaviolube@gmail.com>
2023-02-02 21:53:17 -0300

    Lógica inicial do jogo desenvolvida

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,28 @@
 var cartoes = document.querySelectorAll(".cartao");
 
+var existeCartaoVirado = false;
+var primeiroCartao, segundoCartao;
+
 function virarCartao() {
-  this.classList.toggle("virado");
+  this.classList.add("virado");
+
+  if (existeCartaoVirado == false) {
+    existeCartaoVirado = true;
+    primeiroCartao = this;
+  } else {
+    segundoCartao = this;
+    existeCartaoVirado = false;
+
+    if (primeiroCartao.dataset.info === segundoCartao.dataset.info) {
+      primeiroCartao.removeEventListener("click", virarCartao);
+      segundoCartao.removeEventListener("click", virarCartao);
+    } else {
+      setTimeout(function () {
+        primeiroCartao.classList.remove("virado");
+        segundoCartao.classList.remove("virado");
+      }, 2000);
+    }
+  }
 }
 
 cartoes.forEach(function (cartao) {
