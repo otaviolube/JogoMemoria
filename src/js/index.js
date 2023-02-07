aa10ba7a3b12e9747681fb7c22075ea828b3ba40
Otávio Lube <otaviolube@gmail.com>
2023-02-06 21:57:08 -0300

    Sistema de pontuação

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,52 @@ var cartoes = document.querySelectorAll(".cartao");
 var existeCartaoVirado = false;
 var primeiroCartao, segundoCartao;
 
+var resultadosJogadores = [];
+
+var itensStorage = localStorage.getItem("resultado");
+
+if (itensStorage) {
+  var resultadosExistentes = JSON.parse(itensStorage);
+} else {
+  var resultadosExistentes = [];
+}
+
+console.log(resultadosJogadores);
+
 var placar = 0;
 var tempo = 0;
 
 var intervalo = setInterval(function () {
   if (tempo == 15) {
     clearInterval(intervalo);
+    alert("Tempo esgotado!");
+    var nome = prompt("Digite o seu nome:");
+    //Criar um objeto
+    var resultado = {
+      nomeJogador: nome,
+      pontuacao: placar,
+    };
+
+    resultadosExistentes.push(resultado);
+    localStorage.setItem("resultado", JSON.stringify(resultadosExistentes));
+
+    document.getElementById("recordes").style.display = "block";
+
+    var tabela = document.getElementById("resultados");
+
+    resultadosExistentes.sort().forEach(function (resultado) {
+      var tr = document.createElement("tr");
+      var tdNome = document.createElement("td");
+      tdNome.innerHTML = resultado.nomeJogador;
+      var tdPontuacao = document.createElement("td");
+      tdPontuacao.innerHTML = resultado.pontuacao;
+      tr.appendChild(tdNome);
+      tr.appendChild(tdPontuacao);
+      tabela.appendChild(tr);
+    });
+
     cartoes.forEach(function (cartao) {
-      cartao.addEventListener("click", virarCartao);
+      cartao.removeEventListener("click", virarCartao);
     });
     return;
   }
@@ -34,7 +72,13 @@ function virarCartao() {
       placar = placar + 1;
       document.getElementById("resultado").innerHTML = placar;
     } else {
+      cartoes.forEach(function (cartao) {
+        cartao.removeEventListener("click", virarCartao);
+      });
       setTimeout(function () {
+        cartoes.forEach(function (cartao) {
+          cartao.addEventListener("click", virarCartao);
+        });
         primeiroCartao.classList.remove("virado");
         segundoCartao.classList.remove("virado");
       }, 2000);
